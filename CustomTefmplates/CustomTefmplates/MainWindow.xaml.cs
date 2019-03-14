85a73ca3f401de05c22e67dbe4a9e96b9fc70bdf
freezpl <freezpl@gmail.com>
2019-03-14 23:08:27 +0200

    Complete

--- CustomTefmplates/CustomTefmplates/MainWindow.xaml.cs
+++ CustomTefmplates/CustomTefmplates/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CustomTefmplates.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,7 +25,9 @@ namespace CustomTefmplates
         {
             InitializeComponent();
 
-
+            DataContext = new Autorithation();
+            
         }
+     
     }
 }
---
+++ CustomTefmplates/CustomTefmplates/Models/Autorithation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CustomTefmplates.Models
+{
+    public class Autorithation : IDataErrorInfo
+    {
+        public string Login { get; set; }
+        public string Pass { get; set; }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                string error = String.Empty;
+                switch (columnName)
+                {
+                    case "Login":
+                        if (Login.Length < 6)
+                        {
+                            error = "Login to short!";
+                            break;
+                        }
+                        Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+                        Match match = regex.Match(Login);
+                        if (!match.Success)
+                            error = "Email is not correct!";
+                        break;
+                    case "Pass":
+                        
+                        break;
+                }
+                return error;
+            }
+        }
+
+        public string Error => throw new NotImplementedException();
+
+    }
+}
